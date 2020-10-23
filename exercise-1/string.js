bcb3099b25e6a86a33f37a9b22a34d877cd76d0a
Valentin <valentin77181@gmail.com>
2020-10-23 17:42:05 +0200

    suite exo

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -4,33 +4,76 @@ function ucfirst(str) {
 }
 
 function capitalize(str) {
-	var newStr = [];
-	const arr = str.split(" ");
-	for (var i = 0; i < arr.length; i++) {
-		newStr.push(ucfirst(arr[i]));
-	}
-	return newStr.join(" ");
+	if (typeof(str) !== "string" || str ===  "") return "";
+	return str.split(' ').map(word => ucfirst(word.toLowerCase())).join(' ');
 }
 
 function camelCase(str) {
-	var newStr = "";
-	const arr = str.split(" ");
-	for (var i = 0; i < arr.length; i++) {
-		newStr += ucfirst(arr[i]);
-	}
-	return newStr;
+	if (typeof(str) !== "string" || str ===  "") return "";
+	return capitalize(str).replace(/\W/g, "");
 }
 
 function snake_case(str) {
-	return str.toLowerCase().split(" ").join("_");
+	if (typeof(str) !== "string" || str ===  "") return "";
+	return str.toLowerCase().replace(/\W/g, "_");
 }
 
 function leet(str) {
-	return str.replace(/a/gi, "4").replace(/e/gi, "3").replace(/i/gi, "1").replace(/o/gi, "0").replace(/u/gi, "_").replace(/y/gi, "7");
+	if (typeof(str) !== "string" || str ===  "") return "";
+	return str.replace(/aeiouy/gi, function (e) {
+		switch(e.toLowerCase()) {
+			case 'a':
+				return 4;
+			case 'e':
+				return 3;
+			case 'i':
+				return 1;
+			case 'o':
+				return 0;
+			case 'u':
+				return "(_)";
+			case 'y':
+				return 7;
+		}
+	});
+	//return str.replace(/a/gi, "4").replace(/e/gi, "3").replace(/i/gi, "1").replace(/o/gi, "0").replace(/u/gi, "(_)").replace(/y/gi, "7");
 }
 
-function prop_access(obj) {
-	
+function prop_access(obj, prop) {
+	const props = prop.split(".");
+	var i = 0;
+	for (var j = 0; j < obj.length; j++) {
+		if (obj[j] === props[i]) {}
+	}
 }
 
-console.log(leet("pomme orange banane cours kiwy"));
+function verlan(str) {
+	if (typeof str !== "string" || str === "") return "";
+	return str.split(' ').map(word => word.split("").reverse().join("")).join(" ");
+}
+
+function yoda(str){
+    if (typeof str !== "string" || str === "") return "";
+    return str.split(' ').reverse().join(' ');
+}
+
+function vig(key, string) {
+    if (typeof key !== "string" && key !== "") return ""
+    if (typeof string !== "string" && string !== "") return ""
+
+    const msgLength = string.length
+    const keyLength = key.length
+
+    let cryptage = "";
+
+    for (let i = 1; i < msgLength; i++) {
+        let charCode = string.charCodeAt(i-1)-65
+        let decalage = key.charCodeAt((i-1)%keyLength)-65
+        let char = String.fromCharCode(65+(charCode+decalage)%26)
+
+        cryptage += char
+    }
+
+    return cryptage
+}
+
